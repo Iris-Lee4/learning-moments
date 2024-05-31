efb3b2426725534da1ad0dff753d297c1b6ab1a7
Iris Lee <irislee4git@gmail.com>
2024-05-31 15:10:29 -0400

    adding dropdown in progress

--- src/components/AllPosts.jsx
+++ src/components/AllPosts.jsx
@@ -1,22 +1,68 @@
 import { useEffect } from "react"
 import { useState } from "react"
 import { getAllPosts } from "../services/PostService.jsx"
-import { Container } from "react-bootstrap"
+import { Container, Dropdown } from "react-bootstrap"
+import { getAllTopics } from "../services/TopicService.jsx"
 
 export const AllPosts = () => {
     const [allPosts, setAllPosts] = useState([])
+    const [allTopics, setAllTopics] = useState([])
+    const [filter, setFilter] = useState('')
+    const [filteredPosts, setFilteredPosts] = useState([])
 
-    useEffect(() => {
+    // to get and set posts
+    const getAndSetPosts = () => {
         getAllPosts().then((postsArray) => {
             setAllPosts(postsArray)
         })
+    }
+
+    //to get value of change in dropdown
+    const handleChangeFilter = (event) => {
+        setFilter(event.target.value)
+    }
+
+    //to render posts and topics initially
+    useEffect(() => {
+        getAndSetPosts()
+        getAllTopics().then((topicsArray) => {
+            setAllTopics(topicsArray)
+        })
     }, [])
+
+    // to filter post by topic
+     useEffect(() => {
+         if (filteredPosts) {
+            const filteredPostsArray = allPosts.filter(
+                (post) => post.topicId === filter.id)
+                  setFilteredPosts(filteredPostsArray)
+          } else {
+               setFilteredPosts(allPosts)
+     }
+    },[allPosts])
 
     return (
         <div>
+        <Dropdown>
+            <Dropdown.Toggle
+                onChange={handleChangeFilter}
+            >
+                topic
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                {allTopics.map(topic => {
+                    return (
+                        <Dropdown.Item key={topic.id} value={topic}>
+                            {topic.topic}
+                        </Dropdown.Item>
+                    )
+                })}
+            </Dropdown.Menu>
+        </Dropdown>
+        <div>
             {allPosts.map(post => {
                 return (
-                    <container className="post container" key={post.id}>
+                    <Container className="post container" key={post.id}>
                         <header className="post-title">
                             Title: {post.title}
                         </header>
@@ -38,9 +84,10 @@ export const AllPosts = () => {
                         <div>
                             <span> Liked by </span>{post.likedPosts.length}
                         </div>
-                    </container>
+                    </Container>
                 )
             })}
         </div>
+     </div>
     )
 }
